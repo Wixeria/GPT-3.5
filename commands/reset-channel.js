4444e80c87c2394616b079f0187147d99cf7a03f
Little Jacob <105592273+LittleJJacob@users.noreply.github.com>
2024-03-23 01:25:21 +0400

    Update reset-channel.js

--- commands/reset-channel.js
+++ commands/reset-channel.js
@@ -8,6 +8,16 @@ module.exports = {
 
     run: async (client, interaction) => {
         const guildId = interaction.guild.id;
+        const data = wixdb.get(`chatgpt_${guildId}`);
+
+        if (!data) {
+            return interaction.reply("There's no data for this server.");
+        }
+
+        if (!interaction.member.permissions.has("ADMINISTRATOR")) {
+            return interaction.reply("You must be an administrator to use this command.");
+        }
+
         let allKeys = wixdb.all();
 
         // Check if allKeys is an object, convert it to an array if needed
