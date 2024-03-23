42984b753cc4a18d492f7b7aa0d284efa4aeb626
Little Jacob <105592273+LittleJJacob@users.noreply.github.com>
2024-03-23 14:57:15 +0400

    Update set-chatgpt.js

--- commands/set-chatgpt.js
+++ commands/set-chatgpt.js
@@ -1,6 +1,7 @@
 const { Client, EmbedBuilder, PermissionsBitField } = require("discord.js");
 const wixdb = require("croxydb")
 const Discord = require("discord.js")
+
 module.exports = {
     name: "set-chatgpt",
     description: "Set ChatGPT channel where to chat with OpenAI.",
@@ -15,20 +16,26 @@ module.exports = {
         }
     ],
     run: async (client, interaction) => {
-
+      const guildId = interaction.guild.id;
+      const data = wixdb.get(`chatgpt_${guildId}`);
+      
         const permission = new Discord.EmbedBuilder()
             .setColor("Red")
             .setDescription("You need to have `ManageChannels` permission to use this command.")
+       
+      if (!data) {
+          return interaction.reply(`Channel is already set. ${chatchannel}`);
+      }
 
         const chatchannel = interaction.options.getChannel('channel')
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return interaction.reply({ embeds: [permission], ephemeral: true })
 
-        const basarili = new EmbedBuilder()
+        const succes = new EmbedBuilder()
             .setColor("Green")
             .setDescription(`Chat channel has been set succesfully.\n\nChannel: ${chatchannel}`)
 
         wixdb.set(`chatgpt_${interaction.guild.id}`, { channel: chatchannel.id })
-        return interaction.reply({ embeds: [basarili], ephemeral: true }).catch((e) => { })
+        return interaction.reply({ embeds: [succes], ephemeral: true }).catch((e) => { })
 
     }
 
