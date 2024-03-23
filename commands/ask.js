a3b56cd97b23b4a87c664a2fed0814b2f28572fe
Jacob <105592273+Wixeria@users.noreply.github.com>
2024-03-23 23:30:41 +0400

    Update ask.js

--- commands/ask.js
+++ commands/ask.js
@@ -1,7 +1,6 @@
 const { Client } = require('discord.js');
 const { OpenAI } = require('openai');
 const wixdb = require("croxydb");
-
 module.exports = {
     name: "ask",
     description: "You can ask something to ChatGPT.",
@@ -14,35 +13,27 @@ module.exports = {
         }
     ],
     type: 1,
-
     run: async (client, interaction) => {
       const openai = new OpenAI({ apiKey: process.env.API_KEY });
-
       let data = wixdb.get(`chatgpt_${interaction.guild.id}`);
       if (!data) {
           return interaction.reply({ content: "Can't find channel for ChatGPT. Did you set channel for ChatGPT?", ephemeral: true });
       }
-
       let allowedChannelId = data.channel;
       let currentChannelId = interaction.channelId;
       if (allowedChannelId !== currentChannelId) {
           let allowedChannel = client.channels.cache.get(allowedChannelId);
           return interaction.reply({ content: `Please run this command in ${allowedChannel}.`, ephemeral: true });
       }
-
       let channel1 = data.channel;
       let channel2 = client.channels.cache.get(channel1);
       if (!channel2) return interaction.reply({ content: `Can't find channel for ChatGPT. Did you set channel for ChatGPT?`, ephemeral: true });
-
       const question = interaction.options.getString("ask");
-
       let conversation = [];
-
       conversation.push({
           role: 'system',
           content: question
       });
-
       const response = await openai.chat.completions
           .create({
               model: 'gpt-3.5-turbo',
@@ -52,7 +43,23 @@ module.exports = {
 
       if (!response) {
           return interaction.reply("I'm having some trouble with the OpenAI API. Try again in a moment.");
-      
+      }
+
+      const responseMessage = response.choices[0].message.content;
+      const chunkSizeLimit = 2000;
+
+      const chunkString = (str, chunkSize) => {
+        const chunks = [];
+        for (let i = 0; i < str.length; i += chunkSize) {
+          chunks.push(str.slice(i, i + chunkSize));
+        }
+        return chunks;
+      };
+
+      const messageChunks = chunkString(responseMessage, chunkSizeLimit);
+
+      for (const chunk of messageChunks) {
+        await interaction.reply(chunk);
       }
    }
 };
